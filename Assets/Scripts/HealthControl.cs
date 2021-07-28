ddce17e47ec2de2d6e83542ca62b7e87b56e5d46
ajtc16 <ajtc16@gmail.com>
2021-07-28 08:17:27 -0500

    Subida del proyecto Unity
    se suben todos los componentes del Juego al repositorio Git

---
+++ Assets/Scripts/HealthControl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthControl : MonoBehaviour {
+
+	public GameObject heart1, heart2, heart3;
+	public static int health;
+	public static bool endGame = false;
+	// Use this for initialization
+	void Start () {
+		health = 3;
+		heart1.gameObject.SetActive (true);
+		heart2.gameObject.SetActive (true);
+		heart3.gameObject.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (health > 3) {
+			health = 3;
+		}
+
+		switch (health) {
+		case 3:
+			heart1.gameObject.SetActive (true);
+			heart2.gameObject.SetActive (true);
+			heart3.gameObject.SetActive (true);
+			endGame = false;
+			break;
+		case 2:
+			heart1.gameObject.SetActive (true);
+			heart2.gameObject.SetActive (true);
+			heart3.gameObject.SetActive (false);
+			endGame = false;
+			break;
+		case 1:
+			heart1.gameObject.SetActive (true);
+			heart2.gameObject.SetActive (false);
+			heart3.gameObject.SetActive (false);
+			endGame = false;
+			break;
+		case 0:
+			heart1.gameObject.SetActive (false);
+			heart2.gameObject.SetActive (false);
+			heart3.gameObject.SetActive (false);
+			// cuando todos los corazones se pediron se reinicia el mundo y se pone en0 el marcador
+			endGame = true;
+			break;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float jumpForce = 8f;
+	private Rigidbody2D myBody;
+	bool canJump;
+	float forwardForce = 0f;
+	// Use this for initialization
+	void Start () {
+		myBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Jump()
+	{
+		if (canJump) 
+		{
+			FindObjectOfType<AudioManager>().Play("JumpSound");
+			canJump = false;
+			if (transform.position.x < 0) {
+				forwardForce = 1f;
+			} else
+				forwardForce = 0f;
+			myBody.velocity = new Vector2 (forwardForce, jumpForce);
+		}
+
+
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		canJump = true;
+	}
+}
